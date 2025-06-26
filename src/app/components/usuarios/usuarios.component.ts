f4b549a1484c276eb7440ba1c4a6973d07a09842
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-06-26 12:54:41 -0300

    Update usuarios.component.ts

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment.prod';
 import { createClient } from '@supabase/supabase-js';
 import { Usuario } from '../../models/usuario';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import * as XLSX from 'xlsx';
+import {saveAs} from 'file-saver';
 
 const supabase = createClient(environment.apiUrl, environment.publicAnonKey);
 
 @Component({
   selector: 'app-usuarios',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule,RouterLink],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.scss'
 })
@@ -25,38 +28,56 @@ export class UsuariosComponent implements OnInit{
 
   //trae todos los usuarios a excepcion del logueado
   getUsersData(){
-  supabase.auth.getUser().then(({data,error}) => {
-    if(error){
-      console.error('Error:',error.message);
-      return;
-    }        
-    const adminId = data.user.id;
-
-    supabase.from('usuarios').select('*').neq('id', adminId).then(({data,error}) => {
+    supabase.auth.getUser().then(({data,error}) => {
       if(error){
-        console.error('Error al obtener usuarios:', error.message);
+        console.error('Error:',error.message);
         return;
-      }  
-      console.log('Usuarios:', data);
-      this.usuarios = data;  
+      }        
+      const adminId = data.user.id;
+
+      supabase.from('usuarios').select('*').neq('id', adminId).then(({data,error}) => {
+        if(error){
+          console.error('Error al obtener usuarios:', error.message);
+          return;
+        }  
+        console.log('Usuarios:', data);
+        this.usuarios = data;  
+      });
     });
-  });
-}
+  }
 
-actualizarEstado(usuario: Usuario) {
-  const nuevoEstado = !usuario.habilitado;
+  actualizarEstado(usuario: Usuario) {
+    const nuevoEstado = !usuario.habilitado;
 
-  supabase.from('usuarios')
-    .update({ habilitado: nuevoEstado })
-    .eq('id', usuario.id)
-    .then(({ data, error }) => {
-      if (error) {
-        console.error('Error al actualizar estado:', error.message);
-        return;
-      }
+    supabase.from('usuarios')
+      .update({ habilitado: nuevoEstado })
+      .eq('id', usuario.id)
+      .then(({ data, error }) => {
+        if (error) {
+          console.error('Error al actualizar estado:', error.message);
+          return;
+        }
 
-      usuario.habilitado = nuevoEstado;
-    });
-}
+        usuario.habilitado = nuevoEstado;
+      });
+  }
+
+  descargarExcelUsuarios() {
+    const datosParaExcel = this.usuarios.map(usuario => ({
+      Nombre: usuario.nombre,
+      Apellido: usuario.apellido,
+      Email: usuario.email,
+      DNI: usuario.dni,
+      "Tipo de Usuario": usuario.categoria
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(datosParaExcel);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Usuarios');
+
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(blob, 'usuarios.xlsx');
+  }
 
 }
